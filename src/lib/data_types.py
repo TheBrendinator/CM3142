b77651883268668d30f66e49d231d48a7095e8ee
TheBrendinator <b.scott2@rgu.ac.uk>
2026-03-09 20:43:29 +0000

    feat: add more data types

--- src/lib/data_types.py
+++ src/lib/data_types.py
@@ -5,3 +5,22 @@ class ClimateData(TypedDict):
     time_recorded: str
     humidity: float
     temperature: float
+
+
+class LightingData(TypedDict):
+    time_recorded: str
+
+
+class ServiceData(TypedDict):
+    time_recorded: str
+
+
+class SleepData(TypedDict):
+    time_recorded: str
+
+
+class DisplayData(TypedDict):
+    climate: ClimateData
+    lighting: LightingData
+    service: ServiceData
+    sleep: SleepData
